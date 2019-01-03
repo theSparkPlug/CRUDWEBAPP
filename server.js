181b6234cda832f972a6f1f685a2bbcf2ded8290
theSparkPlug <pradk531@gmail.com>
2019-01-04 03:34:51 +0530

    vds

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname+'/controller'))
 
 //let mongoClient = MongoClient.MongoClient;
 
-let dbUrl = 'mongodb://noob:qwerty12@ds237858.mlab.com:37858/aws_db'
+//let dbUrl = 'mongodb://noob:qwerty12@ds237858.mlab.com:37858/aws_db'
 
 //let dbUrl ='mongodb://localhost:27017';
 
@@ -62,71 +62,71 @@ let path = require("path");
 
 
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
+// app.use(bodyParser.urlencoded({
 
-  extended: true
+//   extended: true
 
-}));
+// }));
 
-//path.join(__dirname+'/view/index.html')
+// //path.join(__dirname+'/view/index.html')
 
-app.get( '/',(req,res)=> { res.send( "hello" ) ; } ) ; 
-
-
-app.get('/names', (req, res) => {
-
-  console.log('Request received!');
-
-  namesDb.collection('names').find({}).toArray((err, result) => {
-
-    if (err) throw err;
-     res.send(result);
-
-  });
-
-});
+// app.get( '/',(req,res)=> { res.send( "hello" ) ; } ) ; 
 
 
+// app.get('/names', (req, res) => {
 
-app.post('/names', (req, res) => {
+//   console.log('Request received!');
 
-  console.log('Got post request!');
+//   namesDb.collection('names').find({}).toArray((err, result) => {
 
-  console.log(req.body);
+//     if (err) throw err;
+//      res.send(result);
 
-  namesDb.collection('names').insertOne(req.body, (err, res) => {
+//   });
 
-    if (err) throw err;
-
-    console.log('Inserted!');
-
-  });
-
- // res.send('Post request sent succesfully!');
-    res.redirect('/');
-});
+// });
 
 
 
-app.delete('/names', (req, res) => {
+// app.post('/names', (req, res) => {
 
-  console.log('Got delete request');
+//   console.log('Got post request!');
 
-  console.log('Delete?' + req.query);
+//   console.log(req.body);
 
-  namesDb.collection('names').deleteOne(req.query, (err, res) => {
+//   namesDb.collection('names').insertOne(req.body, (err, res) => {
 
-    if (err) throw err;
+//     if (err) throw err;
 
-    console.log('Deleted!');
+//     console.log('Inserted!');
 
-  });
+//   });
 
-  res.send('Delete request sent!');
+//  // res.send('Post request sent succesfully!');
+//     res.redirect('/');
+// });
 
-});
+
+
+// app.delete('/names', (req, res) => {
+
+//   console.log('Got delete request');
+
+//   console.log('Delete?' + req.query);
+
+//   namesDb.collection('names').deleteOne(req.query, (err, res) => {
+
+//     if (err) throw err;
+
+//     console.log('Deleted!');
+
+//   });
+
+//   res.send('Delete request sent!');
+
+// });
 
 
 let server = app.listen(app.get('port'), () => {
